80a650d3adfe132e2b30b0f8ba6f02c1019fa76b
Ivan <vanyok.korobka@ukr.net>
2023-08-13 19:53:32 +0300

    prepeared project for working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [dataInfo, setDataInfo] = useState('0');
+  const apiKey = "22ef24fd53de6de7a28b5d8355220d26";
+  const trackNumber = "20450745191462";
+
+  const apiUrl = `https://api.novaposhta.ua/v2.0/json/`;
+
+  useEffect(() => {
+    axios
+    .post(
+      apiUrl,
+      {
+        apiKey: "22ef24fd53de6de7a28b5d8355220d26",
+        modelName: "TrackingDocument",
+        calledMethod: "getStatusDocuments",
+        methodProperties: {
+          Documents: [
+            {
+              DocumentNumber: trackNumber,
+            },
+          ],
+        },
+      },
+      
+    )
+    .then((response) => {
+      const data = response.data;
+      setDataInfo(data);
+
+      if (data && data.success) {
+        const documentStatus = data.data[0];
+        console.log("Статус накладної:", data.data);
+      } else {
+        console.log("Помилка запиту:", data);
+      }
+    })
+    .catch((error) => {
+      console.error("Помилка під час запиту:", error);
+    });
+  }, [])
+  // Виконання запиту до API "Нової Пошти"
+  
+
+  return <div className="App">appp</div>;
 }
 
 export default App;
