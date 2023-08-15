9b2b9af0503dc1d9f39b782d0c48f94a46fa2ec7
Ivan <vanyok.korobka@ukr.net>
2023-08-15 19:36:17 +0300

    added adaptive for OrdersInformation and fix layout for Status Btn in TTN Search Panel

--- src/components/OrdersInfo.js
+++ src/components/OrdersInfo.js
@@ -3,9 +3,9 @@ import { CgCloseR } from 'react-icons/cg';
 const OrdersInfo = () => {
   return (
     <div className="mt-[50px]">
-      <div className="w-full p-[20px] min-h-[250px] rounded-2xl flex flex-row bg-[#373737]">
-        <div className="flex-auto">
-          <div className="flex flex-col max-w-[500px]">
+      <div className="w-full p-[20px] min-h-[250px] rounded-2xl flex flex-col sm:flex-row bg-[#373737]">
+        <div className="flex-auto mr-2">
+          <div className="flex flex-col sm:max-w-[500px]">
             <p className="text-white text-base mb-3">
               <span className="text-[#11e962] mb-[5px]">Номер ТТН:</span>
               <br />
@@ -40,7 +40,7 @@ const OrdersInfo = () => {
             <button className="bg-[#020c2f] text-white font-medium py-[5px] px-[10px] w-auto max-w-[250px] hover:bg-[#454dc0] active:scale-[0.9] transition-all">Очистити дані та історію</button>
           </div>
         </div>
-        <div className="w-[250px] h-[250px] bg-[#475569] self-end rounded-2xl py-3 px-2 flex flex-col items-center">
+        <div className="w-[250px] h-[250px] bg-[#475569] self-start sm:self-end rounded-2xl py-3 px-2 mt-4 sm:mt-0 flex flex-col items-center">
           <h3 className="text-white text-xl">Історія пошуку</h3>
           <div className="flex flex-col items-center my-[9px] overflow-y-auto flex-auto">
             <div className="flex items-center">
--- src/components/OrdersSearchPanel.js
+++ src/components/OrdersSearchPanel.js
@@ -15,7 +15,7 @@ const OrdersSearchPanel = () => {
                 className="w-full bg-transparent border-b-2 border-solid border-white outline-none text-white focus:border-[#11e962] pb-[5px] pl-[0px] pt-[25px] transition-all placeholder:transition-transform focus:placeholder:transition-transform focus:placeholder:translate-y-[-25px] duration-300 focus:placeholder:text-[#11e962] "
               />
             </div>
-            <button className="bg-[#190E6F] hover:bg-[#454dc0] min-w-[150px] py-2 text-white font-base font-medium transition-all active:scale-[0.9]">
+            <button className="bg-[#190E6F] hover:bg-[#454dc0] min-w-[150px] max-w-[200px] py-2 text-white font-base font-medium transition-all active:scale-[0.9]">
               Статус ТТН
             </button>
           </form>
