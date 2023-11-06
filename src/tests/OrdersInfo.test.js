b50634690248ce16fcc50e522f73d4c754bf79fa
Ivan <vanyok.korobka@ukr.net>
2023-11-06 12:41:20 +0200

    make OrdersInfo test

---
+++ src/tests/OrdersInfo.test.js
@@ -0,0 +1,17 @@
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen, fireEvent } from "@testing-library/react";
+import OrdersInfo from "../components/OrdersInfo";
+import { Provider } from "react-redux";
+import store from "../store/store";
+
+test("UI testing", () => {
+  render(
+    <Provider store={store}>
+      <OrdersInfo />
+    </Provider>
+  );
+
+  expect(screen.getByText("Історія пошуку")).toBeInTheDocument();
+
+  expect(screen.getByText("Очистити історію")).toBeInTheDocument();
+});
