2d6bd272014f9075c37db4a0ea66eff5df037c8d
Ivan <vanyok.korobka@ukr.net>
2023-11-07 22:06:35 +0200

    prepare ErrorPage test

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -4,13 +4,15 @@ const ErrorPage = () => {
   const error = useRouteError();
 
   return (
-    <div 
+    <div
       data-testid="error-page"
       className="p-3 absolute w-full h-full flex flex-col items-center justify-center text-white font-bold "
     >
       <h1 className="text-4xl tracking-[1.6px]">Уупс!</h1>
       <p className="text-lg mt-6">Вибачте, щось пішло не так</p>
-      <p className="text-lg mt-1">{error.statusText ?? error.message}</p>
+      <p className="text-lg mt-1">
+        {(error?.statusText ?? error?.message) || "Not Found Info"}
+      </p>
       <NavLink
         to="/"
         className="text-lg mt-5 text-gray-400 hover:text-red-400 transition-colors p-3 border border-gray-400 border-1 border-solid rounded-lg hover:border-red-400 text-center"
---
+++ src/tests/ErrorPage.test.js
@@ -0,0 +1,46 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import ErrorPage from "../pages/ErrorPage";
+import {
+  BrowserRouter,
+  MemoryRouter,
+  createMemoryRouter,
+  RouterProvider,
+} from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+
+describe("ErrorPage testing", () => {
+  test("UI availability test", () => {
+    const router = createMemoryRouter(
+      [{ path: "/some-link", element: <ErrorPage /> }],
+      { initialEntries: ["/", "/some-link"], initialIndex: 1 }
+    );
+
+    render(<RouterProvider router={router} />);
+
+    expect(screen.getByText("Уупс!")).toBeInTheDocument();
+    expect(screen.getByText("Вибачте, щось пішло не так")).toBeInTheDocument();
+    expect(screen.getByText("Not Found Info")).toBeInTheDocument();
+    expect(
+      screen.getByText("Повернутись на головну сторінку")
+    ).toBeInTheDocument();
+  });
+
+  test("NavLink working test", async () => {
+    const user = userEvent.setup();
+
+    const router = createMemoryRouter(
+      [{ path: "/some-link", element: <ErrorPage /> }],
+      { initialEntries: ["/", "/some-link"], initialIndex: 1 }
+    );
+
+    render(<RouterProvider router={router} />);
+
+    const link = screen.getByRole("link", {
+      name: "Повернутись на головну сторінку",
+    });
+
+    await user.click(link);
+    expect(window.location.pathname).toBe("/");
+  });
+});
